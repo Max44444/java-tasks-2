d96e82160571f035801384335720ec5a3ece33e5
Maxim <skorokhodov.maxim.444@gmail.com>
2021-05-13 18:34:33 +0300

    fighting action

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -2,18 +2,146 @@ import { controls } from '../../constants/controls';
 
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
-    // resolve the promise with the winner when fight is over
+
+    const firstFighterState = createFighterState(firstFighter, 'left');
+    const secondFighterState = createFighterState(secondFighter, 'right');
+
+    handleAttackActions(firstFighterState, secondFighterState, resolve);
+    handleCriticalAttack(firstFighterState, secondFighterState, resolve);
+    handleBlockActions(firstFighterState, secondFighterState);
+
   });
 }
 
+function createFighterState(fighter, position) {
+  return {
+    fighter,
+    indicator: document.getElementById(`${position}-fighter-indicator`),
+    health: fighter.health,
+    isBlock: false,
+    canCriticallyAttack: false
+  }
+}
+
+function handleAttackActions(firstFighterState, secondFighterState, resolve) {
+
+  const attack = (attackerState, defenderState) => {
+    if (!attackerState.isBlock && !defenderState.isBlock) {
+      if (attackAction(attackerState, defenderState, getDamage) === 'win') {
+        resolve(attackerState.fighter)
+      }
+    }
+  }
+
+  document.addEventListener('keydown', event => {
+
+    switch (event.code) {
+      case controls.PlayerOneAttack:
+        attack(firstFighterState, secondFighterState);
+        break;
+
+      case controls.PlayerTwoAttack:
+        attack(secondFighterState, firstFighterState)
+        break;
+    }
+
+  });
+}
+
+function handleBlockActions(firstFighterState, secondFighterState) {
+
+  document.addEventListener('keydown', event => {
+    switch (event.code) {
+      case controls.PlayerOneBlock:
+        firstFighterState.isBlock = true;
+        break;
+
+      case controls.PlayerTwoBlock:
+        secondFighterState.isBlock = true;
+        break;
+    }
+  })
+
+  document.addEventListener('keyup', event => {
+    switch (event.code) {
+      case controls.PlayerOneBlock:
+        firstFighterState.isBlock = false;
+        break;
+
+      case controls.PlayerTwoBlock:
+        secondFighterState.isBlock = false;
+        break;
+    }
+  });
+
+}
+
+function handleCriticalAttack(firstFighterState, secondFighterState, resolve) {
+
+  const pressedKeys = new Set();
+
+  const isCombinationPresent = combination => combination.every(key => pressedKeys.has(key));
+
+  const resetCriticalAttackTimer = attackerState => {
+    attackerState.canCriticallyAttack = false;
+    setTimeout(() => attackerState.canCriticallyAttack = true, 10000);
+  }
+
+  const tryAttack = (attackerState, defenderState, combination) => {
+    if (!attackerState.isBlock && attackerState.canCriticallyAttack && isCombinationPresent(combination)) {
+      resetCriticalAttackTimer(attackerState);
+      pressedKeys.clear();
+      if (attackAction(attackerState, defenderState, getCriticalDamage) === 'win') {
+        resolve(attackerState.fighter)
+      }
+    }
+  }
+
+  const keyDownHandler = event => {
+    pressedKeys.add(event.code)
+    tryAttack(firstFighterState, secondFighterState, controls.PlayerOneCriticalHitCombination);
+    tryAttack(secondFighterState, firstFighterState, controls.PlayerTwoCriticalHitCombination);
+  }
+
+  resetCriticalAttackTimer(firstFighterState);
+  resetCriticalAttackTimer(secondFighterState);
+
+  document.addEventListener('keydown', keyDownHandler);
+  document.addEventListener('keyup', event => pressedKeys.delete(event.code));
+
+}
+
+function attackAction(attackerState, defenderState, calcDamage) {
+  const { fighter: attacker } = attackerState;
+  const { fighter: defender } = defenderState;
+
+  const damage = calcDamage(attacker, defender);
+  const possibleDamage = Math.min(defenderState.health, damage);
+  defenderState.health -= possibleDamage;
+  defenderState.indicator.style.width = `${100 / defender.health * defenderState.health}%`;
+
+  return defenderState.health === 0 ? 'win' : 'blocked';
+}
+
+function getCriticalDamage(attacker, defender) {
+  return getDamage({
+    attacker,
+    attack: attacker.attack * 2
+  }, defender);
+}
+
 export function getDamage(attacker, defender) {
-  // return damage
+  const hitPower = getHitPower(attacker);
+  const blockPower = getBlockPower(defender)
+  return Math.max(0, hitPower - blockPower);
 }
 
 export function getHitPower(fighter) {
-  // return hit power
+  const criticalHitChance = Math.random() + 1;
+  return fighter.attack * criticalHitChance;
 }
 
 export function getBlockPower(fighter) {
-  // return block power
+  const dodgeChance = Math.random() + 1;
+  return fighter.defense * dodgeChance;
 }
--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,30 @@
+import { showModal } from './modal'
+import { createElement } from '../../helpers/domHelper';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  const title = 'WINNER';
+  const bodyElement = createElement({
+    tagName: 'div',
+    className: 'modal-body'
+  });
+
+  const img = createElement({
+    tagName: 'img',
+    attributes: { src: fighter.source }
+  })
+
+  const text = createElement({
+    tagName: 'p'
+  });
+  text.textContent = fighter.name;
+
+  bodyElement.append(img, text);
+
+  showModal({
+    title,
+    bodyElement,
+    onClose: () => {
+      window.location.reload();
+    }
+  })
 }
